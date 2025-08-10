4a853b1d5d2d84a82ac3a0a4c73d2fa9363c7474
DIEN Laurine <laurine@Mac-Laurine.lan>
2025-08-10 16:56:21 +0200

    Description de vos modifications

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,7 @@
-// supabase-config.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Remplacez avec vos clés de projet Supabase
-const supabaseUrl = 'https://zisovayurzyrkpmzqcul.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppc292YXl1cnp5cmtwbXpxY3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MzExNTUsImV4cCI6MjA3MDQwNzE1NX0.IS40LYSsLzaL-It1ypRZsvgrifb5vhxMsin5y5nnstk;
+const supabaseUrl = 'https://zisovayuryzrkpmzqu1.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWYtYmxvY2siOiJ6aXNvdmF5dXJ5enJrcG16cXVsaCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzE2NDk2MzY1LCJleHAiOjE3NDgwMzIzNjV9.yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWYtYmxvY2siOiJ6aXNvdmF5dXJ5enJrcG16cXVsaCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzE2NDk2MzY1LCJleHAiOjE3NDgwMzIzNjV9';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
