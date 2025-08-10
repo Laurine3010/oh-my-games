050ab5aa2a2356aa3a97086f5b72a5e6be695cfa
DIEN Laurine <laurine@Mac-Laurine.lan>
2025-08-10 16:27:14 +0200

    ajout des fichiers initiaux de l'application

---
+++ supabase-config.js
@@ -0,0 +1,9 @@
+// supabase-config.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+// Remplacez avec vos clés de projet Supabase
+const supabaseUrl = 'https://zisovayurzyrkpmzqcul.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppc292YXl1cnp5cmtwbXpxY3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MzExNTUsImV4cCI6MjA3MDQwNzE1NX0.IS40LYSsLzaL-It1ypRZsvgrifb5vhxMsin5y5nnstk;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
