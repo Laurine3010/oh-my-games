b5345c86f14157e3565f35300946a974aa3c3c9b
DIEN Laurine <laurine@Mac-Laurine.lan>
2025-10-20 13:44:18 +0200

    description action

---
+++ android/app/src/main/java/com/laurine/ohmygames/MainActivity.java
@@ -0,0 +1,5 @@
+package com.laurine.ohmygames;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/supabase-config.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
+
+// Remplacez les valeurs ci-dessous par les clés de votre propre projet Supabase.
+const supabaseUrl = 'https://zisovayurzyrkpmzqcul.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppc292YXl1cnp5cmtwbXpxY3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MzExNTUsImV4cCI6MjA3MDQwNzE1NX0.IS40LYSsLzaL-It1ypRZsvgrifb5vhxMsin5y5nnstk'; 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
